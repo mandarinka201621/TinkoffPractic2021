b3ae26b08a983cad2cfc63af4c7e574da210eda3
mandarinka201621 <76656505+mandarinka201621@users.noreply.github.com>
2021-08-17 21:40:24 +0300

    new realization splash

--- app/src/main/java/com/example/koshelok/MainActivity.kt
+++ app/src/main/java/com/example/koshelok/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.KoshelOK)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }

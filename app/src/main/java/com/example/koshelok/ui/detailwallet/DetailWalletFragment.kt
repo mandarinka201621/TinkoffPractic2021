60157df3a7e2003ee99795f93838abf5fadfc12f
Alexandr Burdelev <alexbur228@gmail.com>
2021-08-18 10:52:48 +0300

    refactor code

--- app/src/main/java/com/example/koshelok/ui/detailwallet/DetailWalletFragment.kt
+++ app/src/main/java/com/example/koshelok/ui/detailwallet/DetailWalletFragment.kt
@@ -1,26 +1,16 @@
 package com.example.koshelok.ui.detailwallet
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.koshelok.R
 import com.example.koshelok.databinding.FragmentDetailWalletBinding
 
-class DetailWalletFragment : Fragment() {
+class DetailWalletFragment : Fragment(R.layout.fragment_detail_wallet) {
     private val binding by viewBinding(FragmentDetailWalletBinding::bind)
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail_wallet, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -37,7 +27,7 @@ class DetailWalletFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.settingButton -> {
-
+                //TODO сделать кнопку настроек
             }
         }
         return true

4c6e620f4a202c772f11924b8dc03c8144ccfe41
mandarinka201621 <76656505+mandarinka201621@users.noreply.github.com>
2021-08-19 23:44:41 +0300

    bug fixed

--- app/src/main/java/com/example/koshelok/ui/sumoperation/SumOperationFragment.kt
+++ app/src/main/java/com/example/koshelok/ui/sumoperation/SumOperationFragment.kt
@@ -1,11 +1,13 @@
 package com.example.koshelok.ui.sumoperation
 
-import android.graphics.Color
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.koshelok.R
 import com.example.koshelok.databinding.FragmentSumOperationTransactionBinding
@@ -17,37 +19,54 @@ class SumOperationFragment : Fragment(R.layout.fragment_sum_operation_transactio
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setAddTextChangedListener()
-
+        backPressedListener()
+        binding.sumOperationEditText.showKeyboard()
         binding.addSumOperationButton.setOnClickListener {
-            TODO()
+            launchTypeFragment()
         }
+    }
+
+    private fun backPressedListener() {
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack(R.id.detailWalletFragment, false)
+        }
+    }
+
+    private fun launchTypeFragment() {
+        binding.sumOperationEditText.hideKeyboard()
+        TODO()
     }
 
     private fun setAddTextChangedListener() {
         binding.sumOperationEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO()
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) =
+                Unit
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO()
-            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
+                Unit
 
             override fun afterTextChanged(str: Editable) {
                 if (str.toString().trim().isNotEmpty() && str.toString().toInt() > 0) {
                     with(binding.addSumOperationButton) {
-                        setBackgroundResource(R.drawable.button_enabled)
-                        setTextColor(Color.WHITE)
-                        true
+                        isEnabled = true
                     }
                 } else {
                     with(binding.addSumOperationButton) {
-                        setBackgroundResource(R.drawable.button_not_available)
-                        setTextColor(Color.GRAY)
-                        false
+                        isEnabled = false
                     }
                 }
             }
         })
+    }
+
+    private fun View.showKeyboard() {
+        this.requestFocus()
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun View.hideKeyboard() {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }

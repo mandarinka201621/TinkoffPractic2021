ecaaaea65aa2b3f1225f618dcdae1d0d8b2723e4
mandarinka201621 <76656505+mandarinka201621@users.noreply.github.com>
2021-08-20 15:14:53 +0300

    bug fixed

--- app/src/main/java/com/example/koshelok/ui/sumoperation/SumOperationFragment.kt
+++ app/src/main/java/com/example/koshelok/ui/sumoperation/SumOperationFragment.kt
@@ -1,16 +1,16 @@
 package com.example.koshelok.ui.sumoperation
 
-import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.koshelok.R
 import com.example.koshelok.databinding.FragmentSumOperationTransactionBinding
+import com.example.koshelok.extentions.hideKeyboard
+import com.example.koshelok.extentions.showKeyboard
 
 class SumOperationFragment : Fragment(R.layout.fragment_sum_operation_transaction) {
 
@@ -28,7 +28,7 @@ class SumOperationFragment : Fragment(R.layout.fragment_sum_operation_transactio
 
     private fun backPressedListener() {
         binding.toolbar.setNavigationOnClickListener {
-            findNavController().popBackStack(R.id.detailWalletFragment, false)
+            findNavController().popBackStack()
         }
     }
 
@@ -57,16 +57,5 @@ class SumOperationFragment : Fragment(R.layout.fragment_sum_operation_transactio
                 }
             }
         })
-    }
-
-    private fun View.showKeyboard() {
-        this.requestFocus()
-        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
-    }
-
-    private fun View.hideKeyboard() {
-        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }

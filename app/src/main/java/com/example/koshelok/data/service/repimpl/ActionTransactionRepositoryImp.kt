68064fee63e47264e2cc204399ea6c4ae9843521
Alexandr Burdelev <alexbur228@gmail.com>
2021-08-22 23:00:02 +0300

    add response entity

--- app/src/main/java/com/example/koshelok/data/service/repimpl/ActionTransactionRepositoryImp.kt
+++ app/src/main/java/com/example/koshelok/data/service/repimpl/ActionTransactionRepositoryImp.kt
@@ -2,7 +2,7 @@ package com.example.koshelok.data.service.repimpl
 
 import com.example.koshelok.data.service.AppApi
 import com.example.koshelok.data.service.Mapper
-import com.example.koshelok.data.service.api.AnswerServerApi
+import com.example.koshelok.data.service.api.ResponseApi
 import com.example.koshelok.domain.repository.ActionTransactionRepository
 import com.example.koshelok.ui.model.Transaction
 import io.reactivex.rxjava3.core.Single
@@ -13,7 +13,7 @@ class ActionTransactionRepositoryImp @Inject constructor(
     private val appApi: AppApi
 ) : ActionTransactionRepository {
 
-    override fun editTransaction(transaction: Transaction): Single<AnswerServerApi> {
+    override fun editTransaction(transaction: Transaction): Single<ResponseApi> {
         return Single.just(transaction)
             .map { mapper.mapTransactionToTransactionApi(it) }
             .flatMap {
---
+++ app/src/main/java/com/example/koshelok/domain/Response.kt
@@ -0,0 +1,3 @@
+package com.example.koshelok.domain
+
+data class Response(val code: Int)

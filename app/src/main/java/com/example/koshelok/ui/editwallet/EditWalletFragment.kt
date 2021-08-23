3d6fe1b66d09bce5e98711af56c77222b21928ec
mandarinka201621 <76656505+mandarinka201621@users.noreply.github.com>
2021-08-23 10:17:32 +0300

    add functional

--- app/src/main/java/com/example/koshelok/ui/editwallet/EditWalletFragment.kt
+++ app/src/main/java/com/example/koshelok/ui/editwallet/EditWalletFragment.kt
@@ -1,11 +1,45 @@
 package com.example.koshelok.ui.editwallet
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.koshelok.R
+import com.example.koshelok.databinding.FragmentEditWalletBinding
 
-class EditWalletFragment: Fragment(R.layout.fragment_edit_wallet) {
+class EditWalletFragment : Fragment(R.layout.fragment_edit_wallet) {
 
- //   private val binding by viewBinding(FragmentEditWalletBinding::bind)
+    private val binding by viewBinding(FragmentEditWalletBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnCLickEditWalletListener()
+        with(binding) {
+            /*    titleTextView.text = TODO()
+                currencyTextView.text = TODO()
+                limitTextView.text = TODO()*/
+        }
 
+        binding.createWalletButton.setOnClickListener {
+            launchDetailWalletFragment()
+        }
+    }
+
+    private fun launchDetailWalletFragment() {
+        // TODO("сохранить кошелёк и перейти на экрна detailWallet")
+    }
+
+    private fun setOnCLickEditWalletListener() {
+        with(binding) {
+            titleLayout.setOnClickListener {
+
+            }
+            currencyLayout.setOnClickListener {
+
+            }
+            limitLayout.setOnClickListener {
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/koshelok/ui/titlewallet/AddTitleWalletFragment.kt
+++ app/src/main/java/com/example/koshelok/ui/titlewallet/AddTitleWalletFragment.kt
@@ -5,24 +5,18 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.koshelok.R
 import com.example.koshelok.databinding.FragmentAddTitleWalletBinding
 import com.example.koshelok.extentions.hideKeyboard
 import com.example.koshelok.extentions.showKeyboard
-import com.example.koshelok.ui.sumoperation.SumOperationFragmentArgs
-import com.example.koshelok.ui.sumoperation.SumOperationViewModel
 
 class AddTitleWalletFragment : Fragment(R.layout.fragment_add_title_wallet) {
 
     private val binding by viewBinding(FragmentAddTitleWalletBinding::bind)
 
-    private val args by navArgs<SumOperationFragmentArgs>()
-    private val transaction by lazy { args.transaction }
-    private val viewModel: SumOperationViewModel by viewModels()
+    //private val viewModel: SumOperationViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,7 +26,12 @@ class AddTitleWalletFragment : Fragment(R.layout.fragment_add_title_wallet) {
         binding.fartherButton.setOnClickListener {
             launchEditWalletFragment()
         }
-        transaction.let { viewModel.setSumType(it) }
+       TODO("Сохранение wallet в viewModel")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.titleWalletEditText.hideKeyboard()
     }
 
     private fun setOnBackPressedListener() {
@@ -43,8 +42,7 @@ class AddTitleWalletFragment : Fragment(R.layout.fragment_add_title_wallet) {
 
     private fun launchEditWalletFragment() {
         binding.titleWalletEditText.hideKeyboard()
-        //binding.titleWalletEditText.text.toString()
-            TODO("Сделать перехо")
+            TODO("Сделать переход и засесить изменения wallet")
     }
 
     private fun setAddTextChangedListener() {

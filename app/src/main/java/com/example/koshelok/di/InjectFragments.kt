a1d324ad94a5e681019166357101a853c0da71cc
mandarinka201621 <76656505+mandarinka201621@users.noreply.github.com>
2021-08-24 17:52:22 +0300

    add edit wallet creation

--- app/src/main/java/com/example/koshelok/di/InjectFragments.kt
+++ app/src/main/java/com/example/koshelok/di/InjectFragments.kt
@@ -4,6 +4,7 @@ import com.example.koshelok.ui.addoperation.AddOperationFragment
 import com.example.koshelok.ui.categoryoperation.CategoryOperationFragment
 import com.example.koshelok.ui.detailwallet.DetailWalletFragment
 import com.example.koshelok.ui.editwallet.EditWalletFragment
+import com.example.koshelok.ui.limitwallet.LimitWalletFragment
 import com.example.koshelok.ui.listwallet.ListWalletFragment
 import com.example.koshelok.ui.onboarding.OnBoardingScreenFragment
 import com.example.koshelok.ui.sumoperation.SumOperationFragment
@@ -26,4 +27,6 @@ interface InjectFragments {
     fun inject(listWalletFragment: ListWalletFragment)
 
     fun inject(editWalletFragment: EditWalletFragment)
+
+    fun inject(limitWalletFragment: LimitWalletFragment)
 }

fa875ca485a5398290eaadc439c522e31532a241
Alexandr Burdelev <alexbur228@gmail.com>
2021-08-24 13:19:22 +0300

    add server registration

--- app/src/main/java/com/example/koshelok/ui/onboarding/OnBoardScreenViewModel.kt
+++ app/src/main/java/com/example/koshelok/ui/onboarding/OnBoardScreenViewModel.kt
@@ -2,16 +2,15 @@ package com.example.koshelok.ui.onboarding
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.koshelok.data.AccountSharedPreferences
 import com.example.koshelok.domain.Result
 import com.example.koshelok.domain.usecase.RegistrationUserUseCase
 import com.example.koshelok.ui.RxViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
 class OnBoardScreenViewModel @Inject constructor(
     private val registrationUserUseCase: RegistrationUserUseCase,
-    private val accountSharedPreferences: AccountSharedPreferences
 ) : RxViewModel() {
 
     val resultData: LiveData<Result>
@@ -22,9 +21,9 @@ class OnBoardScreenViewModel @Inject constructor(
     fun registrationUser(email: String) {
         registrationUserUseCase(email)
             .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { personId ->
-                    accountSharedPreferences.personId = personId
                     _loadStateData.value = Result.Success<Long>(personId)
                 },
                 { error ->

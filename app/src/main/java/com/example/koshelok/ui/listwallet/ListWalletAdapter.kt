a924cb15b0af9b089e65f3e0e4ea31415b5d35d6
Alexandr Burdelev <alexbur228@gmail.com>
2021-08-26 23:22:39 +0300

    update swipe

--- app/src/main/java/com/example/koshelok/ui/listwallet/ListWalletAdapter.kt
+++ app/src/main/java/com/example/koshelok/ui/listwallet/ListWalletAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.koshelok.ui.listwallet
 
 import android.annotation.SuppressLint
+import android.graphics.PointF
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -13,6 +14,7 @@ import com.example.koshelok.R
 import com.example.koshelok.databinding.ItemWalletBinding
 import com.example.koshelok.ui.listwallet.entity.WalletEntity
 import java.util.*
+import kotlin.math.abs
 
 class WalletListAdapter(
     private val transitionToDetailWallet: (walletId: Long) -> Unit,
@@ -30,10 +32,12 @@ class WalletListAdapter(
 
         viewHolder.itemView.setOnTouchListener(object : View.OnTouchListener {
             private var startClickTime: Long = 0
+            private var downPoint = PointF(0f, 0f)
 
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                when (event!!.action) {
+            override fun onTouch(v: View?, event: MotionEvent): Boolean {
+                when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
+                        downPoint = PointF(event.x, event.y)
                         startClickTime = Calendar.getInstance().timeInMillis
                     }
                     MotionEvent.ACTION_UP -> {
@@ -41,6 +45,7 @@ class WalletListAdapter(
                             Calendar.getInstance().timeInMillis - startClickTime
                         if (clickDuration < MAX_CLICK_DURATION
                             && viewHolder.adapterPosition != RecyclerView.NO_POSITION
+                            && absPointClick(PointF(event.x, event.y), downPoint)
                         ) {
                             transitionToDetailWallet(diffUtil.currentList[viewHolder.adapterPosition].id)
                         }
@@ -50,6 +55,10 @@ class WalletListAdapter(
             }
         })
         return viewHolder
+    }
+
+    private fun absPointClick(upClick: PointF, downClick: PointF): Boolean {
+        return abs(upClick.x - downClick.x) <= ABS_PIXEL && abs(upClick.y - downClick.y) <= ABS_PIXEL
     }
 
     override fun onBindViewHolder(holder: WalletHolder, position: Int) {
@@ -70,7 +79,8 @@ class WalletListAdapter(
     fun isEmptyList() = diffUtil.currentList.isEmpty()
 
     private companion object {
-        const val MAX_CLICK_DURATION = 65
+        const val MAX_CLICK_DURATION = 200
+        const val ABS_PIXEL = 10
     }
 
     inner class WalletHolder(view: View) : RecyclerView.ViewHolder(view) {
